49e36bd3e6a29592a64163213eb29fb3c1e82a0d
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-05 22:40:28 +0200

    Add router and simple header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
