9876b81439e02f81cf65bd50f4334e344e231825
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-06 12:20:10 +0200

    Add login and display user in header
    - Decode access token to get user information
    - Display user data in header
    - useContext

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
 
 const Header = () => {
-  let {name} = useContext(AuthContext);
+  let {user} = useContext(AuthContext);
   return (
     <div>
         <Link to='/'>Home</Link>
         <span> | </span>
-        <Link to='/login'>Login</Link>
-        <p>Hello, {name}</p>
+        {user ? (<Link to='/login'>Logout</Link>): <Link to='/login'>Login</Link>}
+        {user && <p>Hello, {user.username}</p>}
     </div>
   )
 }
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 
 
 
@@ -8,8 +9,36 @@ export default AuthContext;
 
 
 export const AuthProvider = ({ children }) => {
+    let [authTokens, setAuthTokens] = useState(null);
+    let [user, setUser] = useState(null);
+
+    let loginUser = async (e) => {
+        e.preventDefault();
+        let response = await fetch('http://127.0.0.1:8000/api/token/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'username': e.target.username.value, 'password': e.target.password.value})
+
+        })
+        let data = await response.json();
+        if (response.status === 200){
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+        } else{
+            alert('Something went wrong')
+        }
+    }
+
+
+    let contextData = {
+        user: user,
+        loginUser: loginUser,
+
+    }
     return(
-        <AuthContext.Provider value={{'name': 'Denis'}}>
+        <AuthContext.Provider value={contextData}>
                     {children}
         </AuthContext.Provider>
     );
