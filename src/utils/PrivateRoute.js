f210f64a0606d2a2266a3d7cd1a248b8b55da7fa
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-06 12:46:16 +0200

    Update PrivateRoute.js

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,12 +1,11 @@
 import { Navigate } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
-  const authenticated = false;
-
-  if (!authenticated) {
-    return <Navigate to="/login"  />
-}
-  return children;
+    let {user} = useContext(AuthContext);
+    console.log(user)
+    return user ? children : <Navigate to='/login' replace />;
 };
 
 export default PrivateRoute;
